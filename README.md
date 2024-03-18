f20b4dfa8b393465afc50f5da545146c261e8fd1
Alexander Rudenko <rudenko@migdal.science.ru.nl>
2024-03-18 16:30:32 +0100

    More-or-less final version

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Magnetization-Tyablikov
+
+This Julia code calculates temperature-dependent magnetization of an easy-axis 2D ferromagnet within the Tyablikov (RPA) as well as some other approximations.
--- Tyablikov.jl
+++ Tyablikov.jl
@@ -1,50 +1,65 @@
-#!/vol/tcm30/rudenko/julia-1.7.3/bin/julia
+using Polynomials
 using Printf
 using Plots
 using LinearAlgebra
 #using LoopVectorization
 const μB=5.7883818060e-5 # eV/T
-const nkx=50; const nky=nkx
-const J = [10*0.001,0*0.001]; const D=J[1]/10
-const S=3/2
 const kB=8.617333262e-5
+const nkx=50; const nky=nkx
+const J = [10*0.001, 0*0.001]; const D=J[1]/10
+const S=1.0
 const Niter=200
 #const z=[4,4]
 const δ=1e-4
-const latt="triangular"
-const SIA="SSWT" #HP0, HP1, 
-
-#TO-DO:
-
-#println(Threads.nthreads())
+const latt="square"
+const SIA="AC" #HP0, HP1, 
 
 
-function sumbz(a,b)::Float64
-    if a+b >= 1.0
-        return modf(a+b)[1] # frac part of a number
-    elseif a+b < 0.0
-        return modf(a+b+1)[1]
-    else
-        return a+b
+function S_solver(S, ϕ::Float64)
+# This solver finds expectation values <(Sz)> and <(Sz)^2> for a given S and ϕ within the Tyablikov approximation. 
+# ϕ is a function of the magnon spectrum, which also depends on <Sz> and (in some cases) <(Sz)^2>, i.e., for a given magnon spectrum,
+# <Sz> is to be obtained iteratively. 
+# Theory can be found, e.g., in the book "Quantum Theory of Magnetism" by W. Nolting and A. Ramakanth (Springer-Verlag, Berlin, Heidelberg 2009).
+    if !isinteger(S*2)
+        error("ERROR! S is neither integer nor half-integer")
     end
+    if (S>9)
+        error("ERROR! S>9, are you sure? Polynomial coefficients will contain very big numbers. 
+        Numerical errors are unavoidable. (<(Sz)^2>=(<Sz>)^2 is a good approximation for large S.")
+    end
+if S==1/2
+        Sz = S / (1 + 2ϕ)
+        SzSz = S^2
+        return Sz, SzSz
+    elseif S==1
+        Sz = S*(1 + 2ϕ) / (1 + 3ϕ + 3ϕ^2)
+        SzSz = 2*S^2 - (Sz)*(1+2ϕ)
+        return Sz, SzSz
+end    
+Nproj = round(Int,2S+1)
+Pdim = Nproj + 1
+aux=zeros(0)
+for mz in -S:1:S
+    push!(aux,mz)                         # Eq.(7.485)
 end
-@printf("%10.12f\n",sumbz(0,0.999))
-
-
-function loop()
-N=0.0
-
-for kx=1:nkx , ky=1:nky
-for qx=1:nkx , qy=1:nky
-#
-    px = sumbz(kx/nkx, qx/nkx)
-    py = sumbz(ky/nky, qy/nky)
-    N += exp(px)/exp(-py)
-#
+p0 = fromroots(aux)
+Matr = vcat(transpose(collect(p0)))                    # matrix constructed from the polynomial coefficients
+for n = 0 : 1 : round(Int,2*S-1)         
+    m = n + 1
+    coefs_lhs = zeros(Pdim)               # Eq.(7.489)
+    coefs_lhs[m] = S*(S+1)  
+    coefs_lhs[m+1] = -1
+    coefs_lhs[m+2] = -1
+lhs = Polynomial(coefs_lhs)
+rhs = 2*fromroots([0])*fromroots(ones(n)) + ( fromroots(ones(n)) - fromroots(zeros(n)) ) * ( S*(S+1)*Polynomial([1]) - Polynomial([0,1]) - Polynomial([0,0,1]) )
+    coefs_rhs = ϕ * vcat(collect(rhs),zeros(Nproj-m))
+    Matr = vcat(Matr,transpose(coefs_lhs-coefs_rhs))    # append polynomial coefficients to the matrix for each n
 end
-end
-N
-@printf("%10.12f\n",N)
+    A0 = -Matr[:,1]                       # x*A = A0, x[1] = <Sz>, x[2] = <(Sz)^2>, etc.
+    A = Matr[:,2:Pdim]
+    x = inv(A)*A0
+    Sz = x[1] ; SzSz = x[2]
+    return Sz, SzSz
 end
 
 function n0(β,ε)
@@ -61,14 +76,13 @@ function Sz(S,ϕ)
     elseif S==1
         num = S*(1 + 2ϕ)
         den = 1 + 3ϕ + 3ϕ^2
-     #   num = S
-     #   den = 1 + 2ϕ
     else
     num = (S - ϕ)*(1 + ϕ)^(2S+1) + (1 + S + ϕ)*ϕ^(2S+1)
     den = (1 + ϕ)^(2S+1) - ϕ^(2S+1)
     end
     return num/den
 end
+
 
 function γk(x,y)
     gamma = Float64[]
@@ -90,6 +104,7 @@ function γk(x,y)
     return gamma
 end
 
+
 function magnetization(T,method,Bz)
     if (method != "Tyablikov") && 
        (method != "Callen") &&
@@ -109,13 +124,33 @@ global dispersion = zeros(nkx,nkx)
         P0i = 0.0
         Q0 = 0.0
         qi = zeros(size(J,1))
- #       (1 - 2*P0/S) < 0 && (Δ=0) #avoids negative Ωk. single-line 'if' ???
+ #       (1 - 2*P0/S) < 0 && (Δ=0) #avoids negative Ωk. 
   #     (1 - 2*P0/S) < 0 && (P0 = S/2) #avoids negative Ωk. single-line 'if'
+        #
+        if   SIA=="AC"
+       #      SzSz = 2 - σ*(1 + 2*P0)          # this works for S=1 only!
+             SzSz = S_solver(1, P0)[2]
+             Φ = 2*σ*(1 - (S*(S+1) - SzSz) / (2*S^2))
+             ΩSIA = D*Φ
+        elseif SIA=="HP0"
+             ΩSIA = (2S-1)*D
+        elseif SIA=="HP2"
+             ΩSIA = (2S-1)*D*(1-2*P0/S)
+        elseif SIA=="SSWT"
+             ΩSIA = (2S-1)*D*(σ/S)^2
+        else
+             error("Unknown method for SIA decoupling")
+        end
+        #
         #
         if     method=="Tyablikov"
             ν = zeros(size(J,1))
         elseif method=="SSWT"
+ #           if SIA!=="SSWT"&&(method=="SSWT")
+ #               println("WARNING! SSWT method only works with its own SIA decoupling.")
+ #           end
             ν = q
+            ΩSIA = (2S-1)*D*(σ/S)^2
         elseif method=="Callen"
             a = (σ/S)^1 # or (σ/S)^3
             ν = q.*(σ/S)*a
@@ -123,19 +158,6 @@ global dispersion = zeros(nkx,nkx)
             error("Unknown method for magnon dispersion")
         end
         #
-        if     SIA=="AC"
-             SzSz = S*(S+1) - σ*(1 + 2*P0)
-             Φ = 2*σ*(1 - (S*(S+1) - SzSz) / (2*S^2))
-             ΩSIA = D*Φ
-        elseif SIA=="HP0"
-             ΩSIA = (2S-1)*D
-        elseif SIA=="HP1"
-             ΩSIA = (2S-1)*D*(1-2*P0/S)
-        elseif SIA=="SSWT"
-             ΩSIA = (2S-1)*D*(σ/S)^2
-        else
-             error("Unknown method for SIA decoupling")
-        end
         J0 = γk(0,0).*J
         for ikx = 1 : nkx, iky = 1 : nky
             kx = ikx - nkx/2 ; ky = iky - nky/2
@@ -150,17 +172,14 @@ global dispersion = zeros(nkx,nkx)
     #        if (final) 
     #            dispersion[ikx,iky] = Ωk
     #        end
-            if abs(Ωk) < 1/β/1e3
-   #             println(Ωk," ",1/β/1e3," ", n0(β,Ωk)," ",P0i)
-            end
         end
         #
     P0 = P0i
-    Q0 = 1.0 + 2*P0
+#    Q0 = 1.0 + 2*P0
     q = qi
-    method == "SSWT" ? σ = S - P0i : σ = Sz(S,P0)  #σ = S / Q0
+    method == "SSWT" ? σ = S - P0i : σ = Sz(S,P0)  #σ = S / Q0 for S=1/2
     #
-        if (abs(σ-σ_prev)<δ)&&(iter>3)
+        if (abs(σ-σ_prev)<δ) && (iter>3)
             (!final) ? final=true : break
         elseif ((σ<=0)||(σ>S))#||(P0>S*0.9)) 
             σ=0
@@ -172,18 +191,18 @@ global dispersion = zeros(nkx,nkx)
     return σ
 end
 
-@time begin
+function main()
 
-    println(γk(0,0))
+  #  println(γk(0,0))
     println("M=",magnetization(10,"Tyablikov",0.0))
-#    println("dispersion(1,1)=",dispersion[10,10])
     
-    figure = plot(xlabel="Temperature, T (K)", ylabel = "Magnetization, σ", title="M(T), square lattice")
+    figure = plot(xlabel="Temperature, T (K)", ylabel = "Magnetization, σ", title="M(T), S=$S, $latt lattice, $SIA decoupling")
     for method ∈ ["Tyablikov", "SSWT", "Callen"]
         println(method)
-        plot!(figure,1:10:500, T -> magnetization(T,method,0.0), label=method, m=:circle,ls=:solid)
+        plot!(figure, 1:2:220, T -> magnetization(T,method,0.0), label=method, m=:circle,ls=:solid)
     end
     hline!(figure,[0.0,S], color=:black, ls=:dash, label=false)
+    
     figure
     
         
@@ -197,6 +216,16 @@ end
 
     #      loop()
 
+    
+end
+
+@time begin
+
+#    println(S_solver(1, 1.0)[2])
+#    println(Sz(1/2, 1.0) )
+    
+main()
+    
 end
 
 
@@ -207,5 +236,10 @@ end
 
 
 
+
+
+
+
+savefig("figure.png")
 
 
